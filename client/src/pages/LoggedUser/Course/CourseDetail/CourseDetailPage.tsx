9054cf991f4886311dffd57227ffe641e198b12c
Dominik Wojnar <dodo.wojnar@seznam.cz>
2024-05-09 22:20:23 +0200

    styled course detail

--- client/src/pages/LoggedUser/Course/CourseDetail/CourseDetailPage.tsx
+++ client/src/pages/LoggedUser/Course/CourseDetail/CourseDetailPage.tsx
@@ -1,8 +1,11 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
 import axios from "axios";
-import {Button, Container} from "@mui/material";
+import {Box, Button, Container} from "@mui/material";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import AuthContext from "../../../../components/Context/AuthProvider";
+import {CZK} from "../../../../utils/utils";
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 type Video = {
     id: number;
@@ -40,46 +43,130 @@ function CourseDetailPage() {
             const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/course/${courseId}`)
             console.log(response.data)
             setCourse(response.data)
-        }catch(e) {
+            const tmp:{videoId:number, hide:boolean}[] = []
+            response.data?.videos.map((video: any) => {
+                tmp.push({videoId: video.video.id, hide: true})
+            })
+            setVideoHide(tmp)
+
+        } catch (e) {
             console.log(e)
         }
     }
 
     useEffect(() => {
         getCourseDetail()
+        // setVideoHide([
+        //     {
+        //         "videoId": 25,
+        //         "hide": true
+        //     },
+        //     {
+        //         "videoId": 26,
+        //         "hide": true
+        //     },
+        //     {
+        //         "videoId": 27,
+        //         "hide": true
+        //     },
+        //     {
+        //         "videoId": 28,
+        //         "hide": true
+        //     },
+        // ])
+
     }, []);
 
     const {user} = useContext(AuthContext)
     console.log(user)
-    const buyCourse = async() => {
-        try{
+    const buyCourse = async () => {
+        try {
             const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api/course/${courseId}/user/${user.user_id}/create-order`)
             console.log(response)
-        }catch(e){
+        } catch (e) {
             console.log(e)
         }
     }
 
-    return (
-        <Container>
-            <h1>Course detail</h1>
-            <div>{course?.name}</div>
-            <div>{course?.description}</div>
-            <div>{course?.price}</div>
-            {
-                course?.videos.map((video, index) => {
-                    return (
-                        <div key={index}>
-                            <video width="320" height="240" controls>
-                                <source src={`${process.env.REACT_APP_SERVER_URL}/${video.video.video}`} type="video/mp4"/>
-                            </video>
-                        </div>
-                    )
-                })
-            }
 
-            <Button variant={"contained"} color={"success"} onClick={buyCourse}>Buy</Button>
-        </Container>
+    const [videoHide, setVideoHide] = useState<{videoId:number, hide:boolean}[]>([])
+
+    console.log(videoHide)
+    const enrollVideo =  (videoId: number) => {
+    console.log(videoId)
+        setVideoHide(prev => prev.map((videoHide) => {
+            if(videoHide.videoId === videoId) {
+                return {
+                    videoId: videoId,
+                    hide: !videoHide.hide
+                }
+            }
+            return videoHide
+        }))
+
+
+        console.log("clicked")
+    }
+
+    console.log(videoHide)
+    return (
+        <Box>
+            <div className={"bg-green-950 h-56 text-white w-full flex-col "}>
+                <div className={"flex w-full h-16 items-center p-4 text-xs text-gray-400"}>
+                    Course <KeyboardArrowRightIcon fontSize={"small"}/> {course?.topic.name} <KeyboardArrowRightIcon fontSize={"small"} /> {course?.name}
+                </div>
+                <div className={"flex flex-row p-10"}>
+                    <div className={"w-2/3 h-full flex items-center justify-center"}>
+                        <div>{course?.description}</div>
+                    </div>
+
+                    <div className={"w-1/3  flex items-center justify-center flex-col "}>
+                        <div className={"w-64 h-52 bg-black p-3 flex justify-center flex-col items-center"}>
+                            <h1>{course?.name}</h1>
+                            <span
+                                className="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300 w-40">{CZK.format(Number(course?.price))}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={"flex flex-col mt-24 p-5"}>
+                {
+                    course?.videos.map((video, index) => {
+                        return (
+                            <div key={index}>
+                                <div
+                                    className={"border-gray-400 border flex justify-start p-3 hover:bg-gray-200 cursor-pointer"}
+                                    onClick={() => {
+                                        enrollVideo(video.video.id)
+                                    }}>
+                                    <KeyboardArrowDownIcon></KeyboardArrowDownIcon>
+                                    <h1 className={"font-bold"}>Lesson {index}</h1>
+                                </div>
+                                {
+                                    videoHide.find((videoHide) => videoHide.videoId === video.video.id && videoHide.hide) ? (
+                                        <div className={"hidden"} >
+                                            <video width="320" height="240" controls >
+                                                <source src={`${process.env.REACT_APP_SERVER_URL}/${video.video.video}`}
+                                                        type="video/mp4"/>
+                                            </video>
+                                        </div>
+                                    ) : (
+                                        <video width="320" height="240" controls>
+                                            <source src={`${process.env.REACT_APP_SERVER_URL}/${video.video.video}`}
+                                                    type="video/mp4"/>
+                                        </video>
+                                    )
+                                }
+
+
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
+            <Button variant={"contained"} color={"success"} onClick={buyCourse} sx={{width: "200px"}}>Buy</Button>
+        </Box>
     );
 }
 
